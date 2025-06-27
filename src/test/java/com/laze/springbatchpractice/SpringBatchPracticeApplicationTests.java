309f9e0a6d8c61ec29e2a2b2298fb4738bd0c123
L-a-z-e <yysi8771@gmail.com>
2025-06-27 22:40:20 +0900

    spring batch practice

---
+++ src/test/java/com/laze/springbatchpractice/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laze.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
